2f6cab18b37b6840c24edb17ec307cdd75667b4c
Jonathan Jopio <jonathanjopio@gmail.com>
2019-09-29 23:07:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,31 +1,25 @@
 def addition(num1, num2)
   sum  = num1 + num2
-  puts sum
 end
 
 def subtraction(num1, num2)
   difference = num1 - num2
-  puts difference
 end
 
 def division(num1, num2)
   quotient = num1 / num2
-  puts quotient
 end
 
 def multiplication(num1, num2)
   product = num1 * num2
-  puts product
 end
 
 def modulo(num1, num2)
   quotient = num1 / num2
   number = quotient * num2
   remainder = num1 - number
-  puts remainder
 end
 
 def square_root(num)
   answer = Math.sqrt(num)
-  puts answer
 end
